f17010377000296fc2546415acaf420279ba67c9
Nitin <beldarnitin845@gmail.com>
2026-03-03 13:59:51 +0530

    complete the join-call section code

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,5 +1,13 @@
 import { Server } from "socket.io"
 
+
+
+let connection = {}
+let messages = {}
+let timeOnline = {}
+
+
+
 export const connectToSocket = (server) => {
   const io = new Server(server, {
     cors: {
@@ -9,6 +17,41 @@ export const connectToSocket = (server) => {
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id)
+    
+    socket.on("join-call", (path) => {
+      if(connection[path] === undefined){
+        connection[path] = []
+      }
+      connection[path].push(socket.id)
+
+      timeOnline[socket.id] = new Date()
+
+    //  connection[path].forEach(elem  => {
+      //  io.to(elem)
+
+      //})
+
+      for(let a = 0; a < connection[path].length; a++){
+        
+        io.to(connection[path][a]).emit("user-joined", socket.id, connection[path])}
+      
+
+
+      })
+    
+    socket.on("signal", (toId, messages) => {
+      io.to(toId).emit("signal", socket.id,messages)
+    })
+
+    socket.on("chat-message", (data,sender) => {
+
+    })
+
+    socket.on("disconnect", () => {
+
+    })
+    
+
   })
 
   return io
